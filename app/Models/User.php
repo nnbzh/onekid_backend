34ebdba4f18077f07e2ffec98cbdab5786404b20
Nurassyl <nurasyl.bbn@gmail.com>
2021-11-24 12:48:51 +0600

    mutate password

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,7 +44,7 @@ class User extends Authenticatable
     ];
 
     public function setPasswordAttribute($value) {
-        return Hash::make($value);
+        $this->attributes['password'] = Hash::make($value);
     }
 
     public function findForPassport($username) {
